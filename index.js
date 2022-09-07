3abbafab58107073c1cb7b562cf828589fde732b
daniaaqel <dania.aqel@hotmail.com>
2022-09-07 14:41:57 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(array,name){
+    return array.filter((element)=>{
+        return element.toUpperCase() === name.toUpperCase();
+    })
+}
+function fuzzyMatch(array,nameStart){
+    return array.filter((element)=> {
+        return element.startsWith(nameStart);
+    })
+}
+
+function matchName(array, name){
+    return array.filter((element)=>{
+        return element.name === name;
+    })
+}
+
